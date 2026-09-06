83b28776dadaea1fe7742a2f2bf94229e2b329a2
pranaavv <pranavkarens1998@gmail.com>
2026-09-06 15:52:26 +0530

    Added routes for returning short urls

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -52,25 +53,52 @@ func getURL(id string) (URL, error) {
 	return url, nil
 }
 
-func handler(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintln(w,"hello from server")
+func RootPageURL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello from server")
 }
 
+func ShortURLHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		URL string `json:"url"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid request Body", http.StatusBadRequest)
+		return
+	}
+	generatedShortURL := createURL(data.URL)
+	// fmt.Fprintln(w, "Short URL is :- ", generatedShortURL)
+	response := struct {
+		ShortURL string `json:"short_url"`
+	}{ShortURL: generatedShortURL}
 
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
 
+func redirectURL(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/blob/"):]
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+	http.Redirect(w, r, url.OriginalURL, http.StatusFound)
+
+}
 
 func main() {
 	fmt.Println("URL Shortner Starting...")
-	OriginalURL := "https://linkedin.com/in/"
+	OriginalURL := "https://gemini.google.com/app/e43d7583c1ee782e"
 	fmt.Println("URL: ")
 	generateShortURL(OriginalURL)
 	// fmt.Println("URL: ",ShortURL)
 
-	http.HandleFunc("/",handler)
-
+	http.HandleFunc("/", RootPageURL)
+	http.HandleFunc("/shorten", ShortURLHandler)
 	fmt.Println("Starting the server ....")
-	err := http.ListenAndServe(":3000",nil)
-	if err!=nil{
-		fmt.Println("Error while starting server :- ",err)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error while starting server :- ", err)
 	}
 }
